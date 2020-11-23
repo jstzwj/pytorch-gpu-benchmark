e10c823ebcea737a262aee8b5b84007a26ef5f41
jstzwj <1103870790@qq.com>
2020-11-23 12:18:17 +0800

    rtx3090 benchmark

--- test.sh
+++ test.sh
@@ -4,6 +4,6 @@ count=`nvidia-smi --query-gpu=name --format=csv,noheader | wc -l`
 echo 'start'
 for (( c=count; c>=1; c-- ))
 do
-      python3 benchmark_models.py -g $c&&
+      python3 benchmark_models.py -g $c
 done
 echo 'end'
